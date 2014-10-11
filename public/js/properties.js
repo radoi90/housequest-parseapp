127a5b919eb322f51bb4dc5f2526dba3ddbd11b1
Emil <eculic17@gmail.com>
2014-10-11 08:15:51 +0100

    if no location constraint query map frame
    AirBnB style search, useful for browsing map

--- public/js/properties.js
+++ public/js/properties.js
@@ -328,6 +328,14 @@ $(function() {
 
       if (this.model.get("areas").length > 0) {
         query.containedIn("borough", this.model.get("areas"));
+      } else {
+        var SW = this.map.getBounds().getSouthWest();
+            SW = new Parse.GeoPoint(SW.lat(), SW.lng());
+        var NE = this.map.getBounds().getNorthEast();
+            NE = new Parse.GeoPoint(NE.lat(), NE.lng());
+        console.log(SW);
+        console.log(NE);
+        query.withinGeoBox("location", SW, NE)
       }
 
       query.greaterThanOrEqualTo("price_per_month", this.model.get("price_min"));
