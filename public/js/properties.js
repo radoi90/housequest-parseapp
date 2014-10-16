c2b1e07e5054bb6a0dcae6148e99d3ef56b05386
Emil <eculic17@gmail.com>
2014-10-16 01:50:26 +0100

    instantiate fb

--- public/js/properties.js
+++ public/js/properties.js
@@ -41,6 +41,25 @@ $(function() {
   Parse.initialize("5ITlOKP4A8ggw5KYLJnsHYyOoQ9CZydXeUDSqjiQ",
                    "lsm1ZGuKXFw1PLaU6WYHHSLN2o2V6FQd8675nfmi");
 
+  window.fbAsyncInit = function() {
+    Parse.FacebookUtils.init({ // this line replaces FB.init({
+      appId      : 1570124209875630, // Facebook App ID
+      cookie     : true, // enable cookies to allow Parse to access the session 
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.1' // use version 2.1
+    });
+ 
+    // Run code after the Facebook SDK is loaded.
+  };
+ 
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
   // The FeedEntry model is a wrapper to a Listing object
   var FeedEntry = Parse.Object.extend("FeedEntry", {
     defaults: {
