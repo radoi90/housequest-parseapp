040a391ee87e479043dfa5c1cf274f46e68babf9
ceeK <chris.kevin.howell@gmail.com>
2014-10-27 17:15:06 +0000

    Add fix files

--- public/app.js
+++ public/app.js
@@ -4,16 +4,24 @@ $( document ).ready(function() {
   		interval: 3500
 	});
 
-	$("#email-form").submit(function( event ) {
-		var email = $("#email").val();
-		console.log(email);
-
-		var EmailObject = Parse.Object.extend("Email");
-		var emailObject = new EmailObject();
-		emailObject.save({email: email}).then(function(object) {
-		  $(".h5").remove();
-		  $(".h3").text("Great! Look out for our email in a few hours.");
-		  $(".h3").css("color","#27ae60");
-		});
+	$('#email-form').submit(function(event) {
+		event.preventDefault();
+		submit();
 	});
 });
+
+function submit() {
+	var email = $("#email").val();
+	console.log(email);
+
+  $(".h5").remove();
+  $(".h3").text("Great! Look out for our email in a few hours.");
+  $(".h3").css("color","#27ae60");
+  $("#sign-up-box").remove();
+
+	var EmailObject = Parse.Object.extend("Email");
+	var emailObject = new EmailObject();
+	emailObject.save({email: email}).then(function(object) {
+	
+	});
+}
