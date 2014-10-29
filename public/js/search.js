8a46cd33e4e186ea1d8f9adeab5f338348f4890b
Emil <eculic17@gmail.com>
2014-10-29 03:51:32 +0000

    dev api keys

--- public/js/search.js
+++ public/js/search.js
@@ -66,9 +66,15 @@ $(function() {
 
   Parse.$ = jQuery;
 
+  // DEV
   // Initialize Parse with your Parse application javascript keys
-  Parse.initialize("5ITlOKP4A8ggw5KYLJnsHYyOoQ9CZydXeUDSqjiQ",
-                   "lsm1ZGuKXFw1PLaU6WYHHSLN2o2V6FQd8675nfmi");
+  Parse.initialize("xLa1kzoH7j1WfrstOdgYj275dAeFonMwBog7ngNK",
+                 "0wkQGPbLTA6GxW0o2eqDhAui2faUNWXWPExELxuL");
+
+  // PROD
+  // // Initialize Parse with your Parse application javascript keys
+  // Parse.initialize("5ITlOKP4A8ggw5KYLJnsHYyOoQ9CZydXeUDSqjiQ",
+  //                "lsm1ZGuKXFw1PLaU6WYHHSLN2o2V6FQd8675nfmi");
 
   window.fbAsyncInit = function() {
     Parse.FacebookUtils.init({ // this line replaces FB.init({
