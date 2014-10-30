94cef6721cf2ae348cd8c54f4403595ca7932796
Emil <eculic17@gmail.com>
2014-10-30 01:11:01 +0000

    Revert f154bd3..2a1c3e1
    This rolls back to commit f154bd3cddce0c0bf7458f55f2b9a427f99d8644.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+	/* dev */
+	Parse.initialize("xLa1kzoH7j1WfrstOdgYj275dAeFonMwBog7ngNK",
+                 "0wkQGPbLTA6GxW0o2eqDhAui2faUNWXWPExELxuL");
+	/* prod */
 	Parse.initialize("5ITlOKP4A8ggw5KYLJnsHYyOoQ9CZydXeUDSqjiQ", 
 		"lsm1ZGuKXFw1PLaU6WYHHSLN2o2V6FQd8675nfmi");
 	$('.carousel').carousel({
